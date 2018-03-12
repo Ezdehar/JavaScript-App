a22247f38eee12a6d5c4292417cf5872bf2d01ed
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-03-11 23:14:45 -0400

    update

--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -1,96 +1,136 @@
-	$(document).ready(function() {	
+  var panel = $("#quiz-area");
 
-	var correctAnswered = 0;
-  	var incorrectAnswered = 0;
-  	var unanswered =0;
-  	var questionindex =0;
-	var questions = {
-            "question"= "Where was Friends recorded?",
-            "option1": "New York",
-            "option2": "Ohio",
-            "option3": "Chicago",
-            "option4": "California",
-            "answer": "1"
+	var questions = [{
+            question: "Where was Friends recorded?",
+            answers: ["New York", "Ohio", "Chicago", "California"],
+            correctAnswer: "New York"
         }, {
-            "question"= "Which of these two characters dated?",
-            "option1": "Rachel and Ross",
-            "option2": "Joey and Phoebe",
-            "option3": "Rachel and Chandler",
-            "option4": "Ross and Monica",
-            "answer": "1"
+            question: "Which of these two characters dated?",
+            answers: ["Rachel and Ross", "Joey and Phoebe", "Rachel and Chandler", "Ross and Monica"],
+            correctAnswer: "Rachel and Ross"
         }, {
-            "question"= "which character said, 'WE WERE ON A BREAK!'",
-            "option1": "Chandler",
-            "option2": "Joey",
-            "option3": "Ross",
-            "option4": "Rachel",
-            "answer": "3"
+            question: "Which character said, 'WE WERE ON A BREAK!'?",
+            answers: ["Chandler", "Joey", "Ross", "Rachel"],
+            correctAnswer: "Ross"
         }, {
-            "question"= "Which character got stung by a jellyfish?",
-            "option1": "Rachel",
-            "option2": "Joey",
-            "option3": "Monica",
-            "option4": "Chandler",
-            "answer": "3"
+            question: "Which character got stung by a jellyfish?",
+            answers: ["Rachel", "Joey",  "Monica", "Chandler"],
+            correctAnswer: "Monica"
         }, {
-            "question"= "Which character wrote the song Smelly Cat",
-            "option1": "Monica",
-            "option2": "Ross",
-            "option3": "Chandler",
-            "option4": "Phoebe",
-            "answer": "4"
-         }
-        
-$("#New York").on("click", function(response) {
-      for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
- 	  else if;
- 	  	return: to questionindex;
- 	   });
-$("#Rachel and Ross").on("click", function(response) {
-	for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
-      });
- 	  else if 
- 	  	//If the right answer is not picked, the button will not be clicked and returned to answer correctly 
- 	  	return: to questionindex
-$("#Ross").on("click", function(response) {
-      for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
-      });
- 	  else if 
- 	  //If the right answer is not picked, the button will not be clicked and returned to answer correctly 
- 	  	return: to questionindex
-$("#New York").on("click", function(response) {
-      for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
-      });
- 	  else if 
- 	  //If the right answer is not picked, the button will not be clicked and returned to answer correctly 
- 	  	return: to questionindex
-$("#Monica").on("click", function(response) {
-      for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
-      });
- 	  else if 
- 	  //If the right answer is not picked, the button will not be clicked and returned to answer correctly 
- 	  	return: to questionindex
-$("#Phoebe").on("click", function(response) {
-      for (var i = 0; i < questionsArray.length; i++);
-      	//Then render to next question
-      });
- 	  else if 
- 	  //If the right answer is not picked, the button will not be clicked and returned to answer correctly 
- 	  return: to questionindex
- 	});
+            question: "Which character wrote the song Smelly Cat?",
+            answers: ["Monica", "Ross", "Chandler", "Phoebe",],
+            correctAnswer: "Phoebe"
+         }];
 
-}    
+var timer;
 
-	 	//pseudocode:
-	    //Only redner to the next question if the previous question was answered correctly
-	    //Questions need to be answered correctly in order to render to the next question.
-	    //If the right answer is not picked, the button will not be clicked.
-	    //Timer Function for 1 minute.
-	    //There is only 1 minute to answer the whole trivia game.
-	    //Triva game consists of 5 questions
-    
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 60,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>60</span> Seconds</h2>");
+    $("#start").remove();
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+$(document).on("click", "#start", function() {
+  game.start();
+});
+$(document).on("click", "#done", function() {
+  game.done();
+});
+	 
